4e98cdb32e05d3d79378b596055f63ff4411dde8
r.navabian <r.navabian@NAVABIAN>
2023-07-15 15:15:23 +0330

    Add project files.

---
+++ FactoryMethod/ICreditCard.cs
@@ -0,0 +1,51 @@
+﻿namespace FactoryMethod
+{
+    public interface ICreditCard
+    {
+        public string GetCardType();
+        public long GetCreditLimit();
+        public long GetAnnualCharge();
+    }
+
+    public static class CreditCardFactory
+    {
+        public static ICreditCard GetCreditCard(string cardType)
+        {
+            ICreditCard creditCardDetails = null;
+            if (cardType == "")
+            {
+                creditCardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                creditCardDetails = new Titanium();
+            }
+            else if (cardType == "Platnium")
+            {
+                creditCardDetails = new Platnium();
+            }
+
+            return creditCardDetails;
+        }
+
+    }
+
+    public class MoneyBack : ICreditCard
+    {
+        public string GetCardType() => "MoneyBack";
+        public long GetCreditLimit() => 20000;
+        public long GetAnnualCharge() => 5000;
+    }
+    public class Titanium : ICreditCard
+    {
+        public string GetCardType() => "Titanium";
+        public long GetCreditLimit() => 200000;
+        public long GetAnnualCharge() => 50000;
+    }
+    public class Platnium : ICreditCard
+    {
+        public string GetCardType() => "Platnium";
+        public long GetCreditLimit() => 100000;
+        public long GetAnnualCharge() => 30000;
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using FactoryMethod;
+using System.ComponentModel.DataAnnotations;
+using System.Net.WebSockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        ICreditCard cardDetails = CreditCardFactory.GetCreditCard("Platnium");
+
+        if (cardDetails is not null)
+        {
+            Console.WriteLine("CardType: " + cardDetails.GetCardType());
+            Console.WriteLine("CreditLimit: " + cardDetails.GetCreditLimit());
+            Console.WriteLine("AnnualCharge: " + cardDetails.GetAnnualCharge());
+        }
+        else
+        {
+            Console.WriteLine("Invalid credit card type");
+        }
+
+        Console.ReadLine();
+    }
+}
